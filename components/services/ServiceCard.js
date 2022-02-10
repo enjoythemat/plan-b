75dcc860935c6d604a8fe0dee696c629ece0c509
enjoythemat <gg_man91@mail.ru>
2022-02-10 22:43:22 +0500

    sass vars for main colors

--- components/services/ServiceCard.js
+++ components/services/ServiceCard.js
@@ -1,8 +1,13 @@
 import styles from '../../styles/services/ServiceCard.module.scss'
 
-const ServiceCard = ({ title, pic }) => {
+const ServiceCard = ({ title, pic, delay }) => {
   return (
-    <div className={styles.card} style={{backgroundImage: `url("${pic}")`}}>
+    <div
+      className={styles.card}
+      style={{
+        backgroundImage: `url("${pic}")`,
+        animationDelay: `${delay}s`
+      }}>
       <p>{title}</p>
     </div>
   )
--- components/services/Services.js
+++ components/services/Services.js
@@ -4,10 +4,10 @@ import ServiceCard from './ServiceCard'
 
 const Services = () => {
   const services = [
-    { id: 1, title: 'Лазерная резка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://lazer-rmk.ru/wp-content/uploads/2019/03/lazer-rezka.png' },
-    { id: 2, title: 'Гравировка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://r-color.ru/catalog/raskroy-materiala/lazernaya-rezka/lazernaya-rezka.jpg' },
-    { id: 3, title: 'Изготовление', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://proflasermet.ru/assets/images/lra_2_big.jpg' },
-    { id: 4, title: '3D обработка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://images.satu.kz/72430735_w640_h640_lazernaya-rezka-fanery.jpg' }
+    { id: 1, title: 'Лазерная резка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://lazer-rmk.ru/wp-content/uploads/2019/03/lazer-rezka.png', delay: 0.03 },
+    { id: 2, title: 'Гравировка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://r-color.ru/catalog/raskroy-materiala/lazernaya-rezka/lazernaya-rezka.jpg', delay: 0.06 },
+    { id: 3, title: 'Изготовление', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://proflasermet.ru/assets/images/lra_2_big.jpg', delay: 0.09 },
+    { id: 4, title: '3D обработка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://images.satu.kz/72430735_w640_h640_lazernaya-rezka-fanery.jpg', delay: 0.12 }
   ]
 
   return (
@@ -18,9 +18,15 @@ const Services = () => {
       />
 
       <div className={styles.services}>
-        {services.map(({ id, title, subTitle, pic }) => (
-          <ServiceCard key={id} title={title} subTitle={subTitle} pic={pic} />
-        ))}
+        {services.map(({ id, title, subTitle, pic, delay }) => (
+            <ServiceCard
+              key={id}
+              title={title}
+              subTitle={subTitle}
+              pic={pic}
+              delay={delay}
+            />
+          ))}
       </div>
     </>
   )
