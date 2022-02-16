8e95f0c4b3f3455b65f6416e0431adf05a82270b
enjoythemat <gg_man91@mail.ru>
2022-02-16 07:17:10 +0500

    start album works

--- pages/album.js
+++ pages/album.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from '../styles/PAGES/Album.module.scss'
 
 const images = [
@@ -30,18 +31,51 @@ const images = [
   },
   {
     src: 'https://cdn.pixabay.com/photo/2019/03/09/17/30/horse-4044547_960_720.jpg'
-  }
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2020/06/29/00/26/iris-5350997__340.jpg'
+  },
+  {
+    src: 'https://www.eea.europa.eu/themes/biodiversity/state-of-nature-in-the-eu/state-of-nature-2020-subtopic/image_print'
+  },
+  {
+    src: 'https://images.news18.com/ibnlive/uploads/2021/07/1627377451_nature-1200x800.jpg?im=Resize,width=360,aspect=fit,type=normal?im=Resize,width=320,aspect=fit,type=normal'
+  },
+  {
+    src: 'https://nypost.com/wp-content/uploads/sites/2/2021/12/nature_14.jpg?quality=80&strip=all&w=744'
+  },
+  {
+    src: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/279314888.jpg?k=634792d1c74f7c16f71502c6e4f2dd83e56c5745965ea94c3205046ca4736217&o=&hp=1'
+  },
+  {
+    src: 'https://www.verywellhealth.com/thmb/AbVJd0RqySe_EvKFP1jOu6tTB4A=/500x350/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1283080277-b7010f7c094e4fc8bbb6f8cdfd299f7a.jpg'
+  },
 ]
 
 const Album = () => {
+  const [layout, setLayout] = useState('none')
+  const [img, setImg] = useState('')
+
+  const layoutHandler = (e) => {
+    setLayout(prev => {
+      return prev === 'none' ? 'flex' : 'none'
+    })
+    setImg(e.target.currentSrc)
+  }
+
   return (
-    <div className={styles.album}>
-      {images.map((el, index) =>(
-        <div className={styles.img} key={index}>
-          <img src={el.src} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={styles.album}>
+        {images.map((el, index) =>(
+          <div className={styles.img} key={index}>
+            <img src={el.src} onClick={layoutHandler} />
+          </div>
+        ))}
+      </div>
+      <div className={styles.layout} style={{display: `${layout}`}} onClick={layoutHandler}>
+        <img src={img} />
+      </div>
+    </>
   )
 }
 
