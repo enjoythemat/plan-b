b1bbcd41e1490013a6a4058c01fca58628d66a0e
enjoythemat <gg_man91@mail.ru>
2022-02-19 07:24:32 +0500

    new animations

--- components/services/Services.js
+++ components/services/Services.js
@@ -6,15 +6,15 @@ const Services = () => {
   const services = [
     { id: 1, title: 'Лазерная резка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://lazer-rmk.ru/wp-content/uploads/2019/03/lazer-rezka.png', delay: 0.03 },
     { id: 2, title: 'Гравировка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://r-color.ru/catalog/raskroy-materiala/lazernaya-rezka/lazernaya-rezka.jpg', delay: 0.06 },
-    { id: 3, title: 'Изготовление', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://proflasermet.ru/assets/images/lra_2_big.jpg', delay: 0.09 },
+    { id: 3, title: 'Готовые изделия', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://proflasermet.ru/assets/images/lra_2_big.jpg', delay: 0.09 },
     { id: 4, title: '3D обработка', subTitle: 'Lorem ipsum dolor sit.', pic: 'https://images.satu.kz/72430735_w640_h640_lazernaya-rezka-fanery.jpg', delay: 0.12 }
   ]
 
   return (
     <>
       <Heading
-        title="Список услуг"
-        subTitle="Индивидуальный подход к каждому клиенту!"
+        title="Что мы предлагаем"
+        subTitle="Позвоните нам, если не нашли необходимый пункт"
       />
 
       <div className={styles.services}>
